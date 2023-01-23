9f4eb85f1d4635f36ff0a32c79979da62079567b
ve-akanoce <erik.nucibella@vechain.org>
2023-01-23 18:12:48 +0100

    update link in home

--- src/components/Homepage/MeetVeWorld/MeetVeWorld.tsx
+++ src/components/Homepage/MeetVeWorld/MeetVeWorld.tsx
@@ -1,7 +1,9 @@
-import { Button, Heading, Text, VStack } from "@chakra-ui/react"
+import { Button, Heading, Link, Text, VStack } from "@chakra-ui/react"
 import StyledCard from "../../Shared/StyledCard/StyledCard"
+import { WalletSource, WalletSourceInfo } from "../../../model/enums"
 
 const MeetVeWorld = () => {
+  const veWorldSource = WalletSourceInfo[WalletSource.VEWORLD]
   return (
     <StyledCard p={4} h={["auto", "auto", "full"]}>
       <VStack w="full" align={"flex-start"} spacing={4}>
@@ -12,7 +14,9 @@ const MeetVeWorld = () => {
           ease of use in mind.
         </Text>
         <Button variant="link" colorScheme="blue">
-          Discover more
+          <Link href={veWorldSource.url} isExternal>
+            Discover more
+          </Link>
         </Button>
       </VStack>
     </StyledCard>
